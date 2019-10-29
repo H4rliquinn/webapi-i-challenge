601d2cf53605173643564954b1c1e7fba020dba3
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-28 20:57:27 -0400

    Simple server showing hobbit users

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Sup! :(");
+});
+
+server.get("/users", (req, res) => {
+  db.find()
+    .then(ret => {
+      res.status(200).send(ret);
+    })
+    .catch(err => {
+      res.status(500).send("Error: " + err);
+    });
+});
+server.get("/hobbits", (req, res) => {
+  const hobbits = [{ id: 1, name: "Samwise Gamgee" }, { id: 2, name: "Frodo" }];
+  res.status(200).json(hobbits);
+});
+
+server.listen(8000, () => {
+  console.log(`Server listening on port 8000`);
+});
